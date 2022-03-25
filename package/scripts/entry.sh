ee7a96fc109ce23f70ac5ee9a3ad6471f77e4a1b
Dave Goldstein <dpg3k@virginia.edu>
2022-03-25 09:16:20 -0400

    Fix jarfile name

--- package/scripts/entry.sh
+++ package/scripts/entry.sh
@@ -1,7 +1,7 @@
 # set environment
 export PATH=$PATH:/usr/local/openjdk-8/bin
 
-java -classpath "dist/sqsjson2xmlr.jar:lib/*:libaws/*" edu.virginia.sqsjson.SQSQueueDriver
+java -classpath "dist/sqsjson2xml.jar:lib/*:libaws/*" edu.virginia.sqsjson.SQSQueueDriver
 
 #
 # end of file
