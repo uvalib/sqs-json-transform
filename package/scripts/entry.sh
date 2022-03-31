c98d96e798376f5a2fcbb687fa5e96d57d5108b9
Dave Goldstein <dpg3k@virginia.edu>
2022-03-31 11:45:38 -0400

    Support for threads commandline option

--- package/scripts/entry.sh
+++ package/scripts/entry.sh
@@ -1,7 +1,7 @@
 # set environment
 export PATH=$PATH:/usr/local/openjdk-8/bin
 
-java -classpath "dist/sqsjson2xml.jar:lib/*:libaws/*" edu.virginia.sqsjson.SQSQueueDriver
+java -classpath "dist/sqsjson2xml.jar:lib/*:libaws/*" edu.virginia.sqsjson.SQSQueueDriver -threads ${SQS_JSON_TRANSFORM_WORKERS}
 
 #
 # end of file
