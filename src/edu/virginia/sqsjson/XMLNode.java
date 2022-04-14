06c51c8709c0aabb298a50e17880bbf7dd18ae32
Robert Haschart <rh9ec@virginia.edu>
2022-04-14 17:41:19 -0400

    Add code to detect instances where GeoJSON data coordinates are 10000 times too large, and divide all those values by 10000.   When given data in the normal range the data will be unaffected.

--- src/edu/virginia/sqsjson/XMLNode.java
+++ src/edu/virginia/sqsjson/XMLNode.java
@@ -5,6 +5,8 @@ import org.apache.log4j.Logger;
 import java.io.PrintWriter;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class XMLNode {
 
@@ -74,15 +76,37 @@ public class XMLNode {
     private String XMLEncode(String text)
     {
         String result = text;
-        result = text.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">",  "&gt;")
-        		     .replaceAll("&amp;(#[0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f];)", "&$1");
+        if (text.contains("coordinates"))
+        {
+            result = fixCoordinates(text);
+        }
+        result = result.replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">",  "&gt;")
+                       .replaceAll("&amp;(#[0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f];)", "&$1");
 
         String legalResult = result.replaceAll("[\u0001\u0002\u0003\u0004\u0005\u0006\u0007\u0008\u000B\u000C\u000E\u000F]", "")
-        		                   .replaceAll("[\u0010\u0011\u0012\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001A\u001B\u001C\u001D\u001E\u001F]", "");
+                                   .replaceAll("[\u0010\u0011\u0012\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001A\u001B\u001C\u001D\u001E\u001F]", "");
         if (!result.equals(legalResult))
         {
-        	logger.warn("Encountered illegal character(s) in input file in range 0x00 to 0x1F, deleting it (or them)");
+            logger.warn("Encountered illegal character(s) in input file in range 0x00 to 0x1F, deleting it (or them)");
         }
         return legalResult;
+    }
+
+    private String fixCoordinates(String text2)
+    {
+        Pattern coordinatePattern = Pattern.compile("(\\[[-]?\\d*)(\\d\\d\\d\\d)[.](\\d*,[ ]*)([-]?\\d*)(\\d\\d\\d\\d)[.](\\d*])");
+        Matcher coordinateMatcher;
+        int numFixed = 0;
+        int start = 0;
+        for (coordinateMatcher = coordinatePattern.matcher(text2); coordinateMatcher.find(start); coordinateMatcher = coordinatePattern.matcher(text2))
+        {
+            numFixed ++;
+            text2 = coordinateMatcher.replaceFirst("$1.$2$3$4.$5$6");
+        }
+        if (numFixed > 0)
+        {
+        	logger.warn("Encountered and fixed "+numFixed+" instances where the GeoJSON coordinates were 10000 times too large");
+        }
+        return(text2);
     }
 }
